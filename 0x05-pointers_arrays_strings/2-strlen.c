3fe15827e2faac248b40365fe95f04ad2f957379
Adeyemigrace <yemiobatunwase@gmail.com>
2022-09-20 13:21:50 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,10 @@
  * @s: string
  * Return: returns length as integer
  */
-int _strlen( char *s)
+int _strlen(char *s)
 {
-	int len = 0;
-
-	while (*(s + len) != '\0')
+	int len = 0
+		while (*(s + len) != '\0')
 		len++;
 	return (len);
 }
