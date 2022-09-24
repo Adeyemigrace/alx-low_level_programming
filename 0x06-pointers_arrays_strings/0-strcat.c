9c4ba9e3821aaa1f868a8217bc1f1a0454634659
Adeyemigrace <yemiobatunwase@gmail.com>
2022-09-23 22:29:18 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strcat - function that concatenates two strings
- * @dest: pointer to destination input
- * @src: pointer to source input
+ * _strcat - Concatenates the string pointed to by @src,
+ * null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
  *
- * Return: pointer to resulting string @dest
+ * Return: A pointer to the destination string @dest.
  */
 char *_strcat(char *dest, char *src)
 {
-	int c, c2;
-
-	c = 0;
-	/*find the size of dest array*/
-	while (dest[c])
-		c++;
-
-	/*iterate through each src array value without the null byte*/
-	for (c2 = 0; src[c2] ; c2++)
-		/*append src[c] to dest[c] 
-while overwritting the null byte in dest*/
-		dest[c++] = src[c2];
-
+	int index = 0, dest_len = 0;
+	
+	while (dest[index++])
+		dest_len++;
+	
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/_putchar.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, 
